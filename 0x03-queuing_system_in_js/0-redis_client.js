db937a0f49af074736c558bcbb84747c94d099eb
HazelDaniel <hazeldaniel856@gmail.com>
2024-05-08 11:17:11 +0100

    task-0: submission

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+import { createClient } from 'redis';
+
+const redisClient = createClient();
+
+redisClient.on('error', (error) => {
+  console.log(`Redis client not connected to server: ${error.message}`);
+  redisClient.quit();
+});
+
+redisClient.on('connect', () => console.log('Redis client connected to the server'));
